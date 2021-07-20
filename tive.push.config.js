f47fd40b734c945e15ddd9507e8a25aa08f65453
zhouxianfu <zhouxianfu@yiche.com>
2021-07-20 11:41:14 +0800

    优化

--- tive.push.config.js
+++ tive.push.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   shell: [
     'node sitemap-to-txt.js',
-    'curl -H "Content-Type:text/plain" --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://www.tiven.cn&token=QZWCDMydKCiY8doP"',
+    'curl -H "Content-Type:text/plain" --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://tiven.cn&token=99njAs0UcvKK1yWD"',
   ],
 }
