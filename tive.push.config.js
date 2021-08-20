2785f2b731c457241b7a8622bac6f7527ee89506
zhouxianfu <zhouxianfu@yiche.com>
2021-08-20 17:30:11 +0800

    new post

--- tive.push.config.js
+++ tive.push.config.js
@@ -3,5 +3,6 @@ module.exports = {
     'node sitemap-to-txt.js',
     'curl -H "Content-Type:text/plain" --data-binary @sitemap.txt "http://data.zz.baidu.com/urls?site=https://tiven.cn&token=99njAs0UcvKK1yWD"',
     'node script/url-push.js',
+    'node deploy.js',
   ],
 }
